f8c75653e54b5b777ddddc49ccebd23670ff3885
Q300Z <Q300Zhomas@gmail.com>
2025-06-23 13:44:58 +0200

    feat(logging): Implement structured logging with rotation and environment-based log directories

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -17,8 +17,12 @@ func GetRoutes(r *gin.Engine, serviceManager *services.ServiceManager) {
 	v1.AddTrackerRoutes(v1Group, serviceManager)
 	v1.AddUserRoutes(v1Group, serviceManager)
 	v1Group.GET("/version", func(ctx *gin.Context) {
+		// Lire le fichier de version
+		// et renvoyer la version de l'API
+
 		ctx.JSON(200, gin.H{
-			"version": utils.GetEnv("VERSION", "ERROR"),
+			"version": utils.GetVersion(),
+			"mode":    utils.GetEnv("GIN_MODE", "ERROR"),
 			"message": "API is running",
 		})
 	})
